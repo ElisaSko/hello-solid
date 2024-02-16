6ae6c69286443604148e98b7d7b2547f2ffaa3cb
Elisa Skowronek <elisa.skowronek@etu.minesparis.psl.eu>
2024-02-16 16:54:46 +0100

    photos aleatoires

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div>
     <p class="text-4xl text-green-700 text-center py-20">Vert!</p>
     <p class="text-4xl text-blue-700 text-center py-20">Bleu!</p>
+    <img src="https://picsum.photos/200/300" alt="random" />
     </div>
   );
 }
