be38a521c8de937fee5098e8958e6439bf2366b9
Elisa Skowronek <elisa.skowronek@etu.minesparis.psl.eu>
2024-02-16 16:32:24 +0100

    baababab

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div>
-    <p class="text-4xl text-green-700 text-center py-20">Hello les Mines!</p>
-    <p class="text-4xl text-blue-700 text-center py-20">Hello les Mines!</p>
+    <p class="text-4xl text-green-700 text-center py-20">Vert!</p>
+    <p class="text-4xl text-blue-700 text-center py-20">Bleu!</p>
     </div>
   );
 }
