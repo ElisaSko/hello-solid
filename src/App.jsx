ecf0dd5363f5c2ff14398bbde225da9ae60c7f8d
Elisa Skowronek <elisa.skowronek@etu.minesparis.psl.eu>
2024-02-23 20:37:01 +0100

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import './monstyle.css'
+
 function App() {
   return (
     <div>
     <p class="text-4xl text-green-700 text-center py-20">Vert!</p>
+    <p class="bg-blue-300 hover:bg-blue-200">
+      Coucouuuuu
+      <br />
+      Le président de la république de Finlande (en finnois : Suomen tasavallan presidentti ; en suédois : Republiken Finlands president) est le chef de l'État de la Finlande depuis sa création, ainsi qu'il est disposé dans la loi constitutionnelle de 1919. Il exerce, d'après la Constitution de Finlande, le pouvoir exécutif qu'il partage avec le gouvernement. Son mandat est d'une durée de six ans. Depuis 1991, son mandat n'est renouvelable qu'une seule fois, et depuis 1994, il est élu directement par les citoyens au suffrage universel direct. Afin d'être élu, un candidat à la présidence doit être né de citoyenneté finlandaise et avoir vu le jour en Finlande.
+      Sauli Niinistö, élu le 5 février 2012, prête serment le 1er mars suivant, devenant à cette occasion le douzième président de la république de Finlande. Il est réélu dés le premier tour, à la suite de l'élection présidentielle de 2018.
+    </p>
     <p class="text-4xl text-blue-700 text-center py-20">Bleu!</p>
     <img src="https://picsum.photos/200/300" alt="random" />
     </div>
